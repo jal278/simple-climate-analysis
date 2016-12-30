1028db6b06f6272d1f6d152b5c873f3abb5f9221
Joel <jleh@itu.dk>
2016-12-30 10:04:20 -0600

    initial commit

---
+++ load_data.py
@@ -0,0 +1,58 @@
+import pandas
+
+months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+
+import pdb
+
+def load_co2(fname='co2-mm-mlo.csv'):
+ data=pandas.read_csv(fname)
+ year_decimal=data['Decimal Date']
+ co2_decimal=data['Trend']
+ data = pandas.DataFrame({'year_decimal':year_decimal,'co2':co2_decimal})
+ return data
+
+def load_temp(fname='GLB.Ts.csv'):
+ #this data gives one row per year, 12 columns with temperature per month
+ data=pandas.read_csv(fname,skiprows=1)
+
+ year_decimal = []
+ temp_decimal = []
+
+ #loop through rows of data file
+ for idx,row in data.iterrows():
+ 
+  #grab year
+  year = row['Year']
+ 
+  #there's a column for each month
+  for m_idx,month in enumerate(months):
+   #get temperature data for month
+   data = row[month]
+
+   try:
+    #convert temperature to floating point number
+    data = float(data)
+    #create floating point year: 
+    #January 2012 = 2012
+    #February 2012 = 2012 + 1/11
+    #etc. for easier plotting
+    year_decimal.append(year+float(m_idx)/11.0)
+    temp_decimal.append(data)
+   except:
+    #at the end of the current year, i.e. the future, we won't have data
+    print "column not available:",year,month
+ 
+ data = pandas.DataFrame(data={'year_decimal':year_decimal,'temp':temp_decimal})
+ return data
+
+
+co2_data = load_co2()
+temp_data = load_temp()
+
+from pylab import *
+subplot(211)
+plot(co2_data['year_decimal'],co2_data['co2'])
+subplot(212)
+plot(temp_data['year_decimal'],temp_data['temp'])
+show()
+
